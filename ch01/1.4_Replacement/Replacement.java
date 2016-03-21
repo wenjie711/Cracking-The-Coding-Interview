1034b6b51972e01327719486c23e87908693112b
wenjie711 <411764740@qq.com>
2016-03-21 14:28:27 +0800

    update 1.4

--- ch01/1.4_Replacement/Replacement.java
+++ ch01/1.4_Replacement/Replacement.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Replacement {
+    // http://www.nowcoder.com/
     public String replaceSpace(String iniString, int length) {
         // write code here
         int cnt = 0;
@@ -22,8 +23,43 @@ public class Replacement {
         return new String(c);
     }
 
+    // http://www.lintcode.com/zh-cn/problem/space-replacement/
+    public int replaceBlank(char[] string, int length) {
+        // Write your code here
+        int cnt = 0;
+        for (int i = 0; i < length; i++) {
+            if (string[i] == ' ') {
+                ++cnt;
+            }
+        }
+        int newLen = length + cnt * 2 - 1;
+        for (int i = length - 1; i >= 0; i--) {
+            if (string[i] == ' ') {
+                string[newLen--] = '0';
+                string[newLen--] = '2';
+                string[newLen--] = '%';
+            } else {
+                string[newLen--] = string[i];
+            }
+        }
+        return length + cnt * 2;
+    }
+
     public static void main(String[] args){
         Replacement r = new Replacement();
         System.out.println(r.replaceSpace("nihao ds d", 10));
+
+        char[] t = new char[100];
+        t[0] = 'n';
+        t[1] = 'i';
+        t[2] = 'h';
+        t[3] = 'a';
+        t[4] = ' ';
+        t[5] = 'd';
+        t[6] = 's';
+        t[7] = ' ';
+        t[8] = 'd';
+        System.out.println(r.replaceBlank(t, 10));
+        System.out.println(new String(t));
     }
 }

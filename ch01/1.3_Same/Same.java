61d49659d1bf9759f6be9002d8ab18f679d4f440
wenjie711 <411764740@qq.com>
2016-03-21 14:04:04 +0800

    update 1.3

--- ch01/1.3_Same/Same.java
+++ ch01/1.3_Same/Same.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 public class Same {
+
+    //Solution 1
     public String sort(String s) {
         char[] t = s.toCharArray();
         Arrays.sort(t);
@@ -15,8 +17,24 @@ public class Same {
         return sort(stringA).equals(sort(stringB));
     }
 
+    //Solution 2
+    public boolean checkSam2(String s, String t) {
+        // write your code here
+        int[] hash = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i)]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if ((--hash[t.charAt(i)]) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Same s = new Same();
         System.out.println(s.checkSam("This is nowcoder","is This nowcoder"));
+        System.out.println(s.checkSam2("This is nowcoder","is This nowcoder"));
     }
 }
